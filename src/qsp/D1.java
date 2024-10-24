faf1df486dfc51b676745391eea08460cf445daa
Santhosh <santhoshonnegowda@gmail.com>
2024-10-24 23:19:38 +0530

    first commit

---
+++ src/qsp/D1.java
@@ -0,0 +1,16 @@
+package qsp;
+
+import org.testng.annotations.Test;
+
+public class D1 {
+	@Test
+	public void test()
+	{
+		System.out.println("hello");
+	}
+	@Test
+	public void test1()
+	{
+		System.out.println("world");
+	}
+}
---
+++ src/qsp/D2.java
@@ -0,0 +1,12 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class D2 {
+	@Test
+	public void test3()
+	{
+		Reporter.log("country",true);
+	}
+}
